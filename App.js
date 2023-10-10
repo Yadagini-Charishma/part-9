f4b175a6782836cc370920d5cd003752d3707970
Yadagini-Charishma <116977111+Yadagini-Charishma@users.noreply.github.com>
2023-10-11 00:06:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UserData from './components/UserData';
+import CreatePost from './components/CreatePost';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UserData/> */}
+      <CreatePost/>
+    </div>
+  );
+}
+
+export default App;
